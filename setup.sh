3791a3cf12d42bfec4d08d74d258124a4c860e5a
UseTheForceKyle <64609618+UseTheForceKyle@users.noreply.github.com>
2020-05-02 12:28:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #Log in, get all source from your Sandbox.
 sfdx force:auth:web:login
 cd metadata
-sfdx force:source:retrieve --manifest=AllSourcePackage.xml
+sfdx force:source:retrieve --manifest=./AllSourcePackage.xml
